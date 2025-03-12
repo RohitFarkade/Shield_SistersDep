3d1cf8602b197c2a63344f86e8ddbdb1677ba33a
Rohit <farkaderohit2003@gmail.com>
2025-03-12 19:27:44 +0530

    testing some changes
    new methods

--- src/models/User.js
+++ src/models/User.js
@@ -1,18 +1,56 @@
+// import mongoose from 'mongoose';
+// import bcrypt from 'bcrypt';
+
+// const userSchema = new mongoose.Schema({
+//     fullname: { type: String, required: true },
+//     email: { type: String, required: true, unique: true },
+//     phone: {
+//         type: String,
+//     },
+//     address: {
+//         type: String,
+//     },
+//     password: { type: String, required: true },
+//     otp: { type: String, default: null },
+//     otpExpiry: { type: Date, default: null },
+// });
+
+// // Hash the password before saving
+// userSchema.pre('save', async function (next) {
+//     if (this.isModified('password')) {
+//         this.password = await bcrypt.hash(this.password, 10);
+//     }
+//     next();
+// });
+
+// // Method to compare password
+// userSchema.methods.comparePassword = function (password) {
+//     return bcrypt.compare(password, this.password);
+// };
+
+
+// const User = mongoose.model('User', userSchema);
+// export default User;
+
+
+
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
+    userId: { type: String, required: true, unique: true }, 
     fullname: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    phone: {
-        type: String,
-    },
-    address: {
-        type: String,
-    },
+    phone: { type: String },
+    address: { type: String },
     password: { type: String, required: true },
     otp: { type: String, default: null },
     otpExpiry: { type: Date, default: null },
+    sharedWith: [{ type: String }], // List of userIds allowed to see live location
+    contacts: [{
+        name: { type: String, required: true },
+        phone: { type: String, required: true }
+    }] // Store contacts properly
 });
 
 // Hash the password before saving
@@ -27,7 +65,6 @@ userSchema.pre('save', async function (next) {
 userSchema.methods.comparePassword = function (password) {
     return bcrypt.compare(password, this.password);
 };
-
 
 const User = mongoose.model('User', userSchema);
 export default User;
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -14,5 +14,6 @@ router.delete('/delete', authMiddleware, AuthController.deleteProfile); // Delet
 router.post('/send-otp', AuthController.sendResetOTP);
 router.post('/verify-otp', AuthController.verifyResetOTP);
 router.post('/reset-password', AuthController.resetPassword);
+router.post("/get-location", AuthController.getLocation);
 
 export default router;
