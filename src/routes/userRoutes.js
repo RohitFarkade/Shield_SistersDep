9ea2399e408810448fa6503d89e1bb09d97a0829
Rohit <farkaderohit2003@gmail.com>
2025-03-16 19:56:12 +0530

    n

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -15,6 +15,7 @@ router.post('/send-otp', AuthController.sendResetOTP);
 router.post('/verify-otp', AuthController.verifyResetOTP);
 router.post('/reset-password', AuthController.resetPassword);
 router.get('/getcontacts', AuthController.getContacts);
+router.get('/api/sos/getcontacts', AuthController.getContacts);
 router.put('/updatecontact', AuthController.updateContact);
 router.delete('/deletecontact', AuthController.deleteContact);
 
